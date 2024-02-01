ccb988137ea5be109d4b3f102e1143ec1bdbe02e
Weibin Zhao <weibin.zhao@aspentech.com>
2024-02-01 17:56:24 +0800

    react test

---
+++ react-components/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './router/index';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>Header</header>
+      {/* <RouterProvider router={router} /> */}
+    </div>
+  );
+}
+
+export default App;
